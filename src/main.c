46788b0acdb52fe0b38f8514575d5853acf2ac96
Juan García Garland <jpgarcia@fing.edu.uy>
2018-06-03 22:15:31 -0300

    bugfix: now compiles correctly

--- src/main.c
+++ src/main.c
@@ -1,4 +1,21 @@
+/* Main.c - The main file of garlix */
+/* Copyright (C) 2017,2018 Juan Garcia Garland <jpgarcia@fing.edu.uy>.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 int
-main (unsigned int magic, multiboot_info_t *mboot)
+main (unsigned int magic, int/*multiboot_info_t*/ *mboot)
 {
 }
